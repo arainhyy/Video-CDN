82a88ca48f371d866fa5afb0fc7c3f1adbf4f6f3
Yayun Huang <arainhyy@gmail.com>
2016-11-27 06:49:58 -0500

    Add some API. 1. Add request type check and replace uri API to parse module 2. Add throughput estimate function.

--- parse.h
+++ parse.h
@@ -1,6 +1,3 @@
-#ifndef __PARSE_H__
-#define __PARSE_H__
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +5,7 @@
 
 #define SUCCESS 0
 #define NEEDMORE -1
+
 //Header field
 struct Request_header
 {
@@ -31,31 +29,12 @@ typedef struct
 	int content_readed; // For POST method.
 	int status;
 	int position;
-	int is_cgi;
 	char content_type[50];
 	char query[1024];
 	char* post_body;
 } Request;
 
-typedef struct browser {
-	// browser request information
-	int fd;
-	Request *header;
-	req_uri_t type;
-} browser_t;
-
-typedef struct server {
-	// server response information
-	int fd;
-	Request *header;
-	resp_uri_t type;
-} server_t;
-
 Request* parse(char *buffer, int size,int socketFd);
-
-// parse browser request, get type of request
-int browser_parse_request(browser_t *req);
-// parse browser request, get type of response
-int server_parse_response(server_t *resp);
-
-#endif /* !__PARSE_H__ */
+int check_type(Request* request);
+int replace_f4m_to_nolist(char* f4m_request);
+int replace_uri_bitrate(char* request, int bitrate);
